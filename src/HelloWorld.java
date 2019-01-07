d6e3b0e0caa93d2295f5a2197451f90056fbd7e3
Leszek <leszekstepien@o2.pl>
2019-01-07 20:02:59 +0100

    Dodanie class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" hello World");
 
-        System.out.println("testy");
+        System.out.println("testy i testy");
     }
 }
